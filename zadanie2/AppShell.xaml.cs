f273c559ca4ccd978e33bcf11aac3d532e43606c
Dawid-zsp3 <uT_3772@zsp3zamosc.pl>
2025-03-04 13:00:41 +0100

    Dodaj pliki projektów.

---
+++ zadanie2/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace zadanie2
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ zadanie2/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿namespace zadanie2
+{
+    public partial class MainPage : ContentPage
+    {
+       
+
+        public MainPage()
+        {
+            InitializeComponent();
+           
+        }
+
+        private void OnResetButtonClicked(object sender, EventArgs e)
+        {
+            mojSlider.Value = 50;
+        }
+        private void OnFontSizeChange(object sender, ValueChangedEventArgs e)
+        {
+           mylabel.FontSize = e.NewValue;
+        }
+        private void OnBackgroundColorChange(object sender, ValueChangedEventArgs e)
+        {
+            double value = e.NewValue;
+            this.BackgroundColor = Color.FromRgb(value, value, value);
+        }
+        private void OnFontSizeStepperChange(object sender, ValueChangedEventArgs e)
+        {
+            mylabel.FontSize = e.NewValue;
+        }
+
+
+    }
+
+}
